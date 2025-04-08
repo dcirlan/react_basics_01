1e954da61232aae6112e915f5c9ef4f942ba2758
Denis <kirlan@hotmail.fr>
2025-04-08 13:41:20 +0200

    feat: ajout de la destructuration des props dans PokemonCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import "./App.css";
-import "./components/PokemonCard";
+
 import PokemonCard from "./components/PokemonCard";
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+    imgSrc: "",
+  },
+];
 
 function App() {
   return (
     <div>
-      <PokemonCard />
+      <PokemonCard pokemon={pokemonList[0]} />
     </div>
   );
 }
