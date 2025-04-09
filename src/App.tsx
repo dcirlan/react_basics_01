5c351c9a8c24ab84f82e57ab921f42ba8be0593a
Denis <kirlan@hotmail.fr>
2025-04-09 08:29:17 +0200

    feat: ajout des boutons de navigation entre les Pokémons

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 
 import PokemonCard from "./components/PokemonCard";
@@ -14,9 +15,21 @@ const pokemonList = [
 ];
 
 function App() {
+  const [pokemonName, setPokemonName] = useState("bulbasaur");
+
+  const pokemon = pokemonList.find((pokemon) => pokemon.name === pokemonName);
+
+  if (pokemon == null) {
+    throw new Error("Invalid pokemon name");
+  }
+
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]} />
+      <PokemonCard pokemon={pokemon} />
+      <div>
+        <button onClick={() => setPokemonName("bulbasaur")}>bulbasaur</button>
+        <button onClick={() => setPokemonName("mew")}>mew</button>
+      </div>
     </div>
   );
 }
